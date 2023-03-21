709d017e2f922d8d3e4dfbb2fa3e7cd4f887508b
bouraki <bouraki655@gmaiL.com>
2023-03-21 11:52:14 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Mynav from './components/layout/navbar';
+import Formulaire from './components/page/Formulaire';
+function App() {
+  return (
+    <><Mynav /><Formulaire /></>
+
+  );
+}
+
+export default App;
